a2b5b8ce68c3ad5d4cf6ba0af6c131aa2c7fb3b6
quoctrongdev-prog <quoctrongdev@gmail.com>
2026-08-31 00:33:33 +0700

    Feat: Refresh Token, Auth Middleware

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -6,22 +6,20 @@ import { UserToken } from "../config/model.js";
 dotenv.config();
 
 const generateAccessToken = async (userId: string) => {
-  const token = jwt.sign(
-    { userId },
-    process.env.JWT_ACCESS_SECRET as string,
-    { expiresIn: "15min" },
-  );
-  console.log('token:', token)
+  const token = jwt.sign({ userId }, process.env.JWT_ACCESS_SECRET as string, {
+    expiresIn: "15min",
+  });
+  console.log("token:", token);
   return token;
 };
 
 const generateRefreshToken = async (userId: string) => {
-  const token = jwt.sign(
-    { userId },
-    process.env.JWT_REFRESH_SECRET as string,
-    { expiresIn: "1d" },
-  );
-  return token;
+  const expiredAt = new Date();
+  expiredAt.setDate(expiredAt.getDate() + 7);
+  const token = jwt.sign({ userId }, process.env.JWT_REFRESH_SECRET as string, {
+    expiresIn: "7d",
+  });
+  return { token, expiredAt };
 };
 
 export { generateAccessToken, generateRefreshToken };
