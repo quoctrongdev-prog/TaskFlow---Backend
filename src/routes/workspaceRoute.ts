54682871572a97f8e3956afdb6062a337b439f46
quoctrongdev-prog <quoctrongdev@gmail.com>
2026-09-15 00:05:31 +0700

    Feat: Get workspace, update workspace, update role

--- src/routes/workspaceRoute.ts
+++ src/routes/workspaceRoute.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import authMiddleware from "../middlewares/authMiddleware.js";
-import { createWorkspace } from "../controllers/workspaceController.js";
+import { createWorkspace, getWorkspace, workspaceDetail } from "../controllers/workspaceController.js";
 
 const router = express.Router();
 
 router.post("/create-workspace", authMiddleware, createWorkspace);
+router.get("/get-workspaces", authMiddleware, getWorkspace);
+router.get("/workspace-detail", authMiddleware, workspaceDetail);
 
 export default router;
